a62dcc7524666cbc180d2868bc80102297a9b361
NagashreeHN <hn.nagashree@yahoo.in>
2018-09-14 17:19:25 +0530

    Tests on Search page added

---
+++ src/main/java/com/qa/pages/SearchPage.java
@@ -0,0 +1,7 @@
+package com.qa.pages;
+
+public class SearchPage {
+public void search(){
+	System.out.println("the user is on the search page");
+}
+}
---
+++ src/test/java/com/qa/tests/SearchTest.java
@@ -0,0 +1,10 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+public class SearchTest {
+@Test
+public void search(){
+System.out.println("Search ended here are your reuslts");
+}
+}
